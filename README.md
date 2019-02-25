0d65a24272179ab641381966cded989c2b58dfde
Julian Garcia <jlmurraygarcia@gmail.com>
2019-02-25 12:55:47 -0800

    Stylistic Update and README update. Check it out! JMG

--- README.md
+++ README.md
@@ -32,6 +32,11 @@ My additions to this project started by [Maximegris](https://github.com/maximegr
 - [Components Folder](src/app/components/)
 - [APOD API Provider Class](src/app/providers/apod-api.service.ts)
 
+### Screenshot
+![Version 0](src/assets/APOD-Calendar-ver0.png)
+
+Here you can see the home view of the APOD-Calendar App. Each APOD has its own small card that displays data about the photo. The cards can be clicked to display a scrollable summary of the image, as represented by the 3 card over in the top row of this image. If a photo's image resource cannot be found, it will be represented with a placeholder image like the far right image on the bottom row.
+
 ### Credits
 - As an avid consumer of astronomy photos and news, I was delighted to find out NASA had opened up some of their troves of data to the public via APIs. The data set used in this project is pulled from [here](https://api.nasa.gov/api.html#apod). Thank you NASA for continuing to inspire generations to come. We've only just started exploring.
 - The original boilerplate author's project can be found [here](https://github.com/maximegris/angular-electron.git). Thank you, [Maximegris](https://github.com/maximegris) :)
@@ -39,5 +44,6 @@ My additions to this project started by [Maximegris](https://github.com/maximegr
 - [Angular](https://angular.io)
 - [Electron](https://electronjs.org/)
 - [npm](https://www.npmjs.com/)
+- [Placeholder Images](https://placeholder.com/)
 
 [![Angular Logo](https://www.vectorlogo.zone/logos/angular/angular-icon.svg)](https://angular.io/) [![Electron Logo](https://www.vectorlogo.zone/logos/electronjs/electronjs-icon.svg)](https://electronjs.org/)
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,8 +4,10 @@ import { Observable } from 'rxjs';
 import * as Date from 'date-format';
 
 import { ApodApiService } from '../../providers/apod-api.service';
-import { ApodCardComponent } from '../apod-card/apod-card.component';
 
+/**
+* The Main component of the APOD Calendar
+*/
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,7 +15,6 @@ import { ApodCardComponent } from '../apod-card/apod-card.component';
 })
 export class HomeComponent implements OnInit {
 
-  // temporary
   apodCards: [] = [];// TODO decouple ApodCardComponent
   apod: Object;
 
@@ -26,13 +27,22 @@ export class HomeComponent implements OnInit {
     this.getAPOD();
   }
 
-  // TODO: Show the last 10 days by default; temporarily hard coded date
+  /**
+  * Requests the Astronomy Picture of the Date with the option of
+  * providing a start date in order to return the APODs from
+  * that date until today.
+  *
+  * @param startDate    The Earliest APOD date to request for
+  */
   getAPOD(startDate: string = "2019-02-16") {
+    // TODO: Show the last 10 days by default; temporarily hard coded date above
     // if no start date was provided, let's get today's date
     if (startDate == "") {
       startDate = Date('yyyy-MM-dd').toString();
     }
 
+    // use the angular service to make the API request,
+    // then update the home view
     this._apodService.getAPOD(startDate).subscribe(
       data => { this.apod = data },
       err => console.error(err),
@@ -40,21 +50,31 @@ export class HomeComponent implements OnInit {
     );
   }
 
+  /**
+  * Update the `apodCards` property of this component
+  * in order to rerender the component
+  */
   updateCalendar(): void {
     // create data array
     var data = [];
     // concat the returned data;
     data = data.concat(this.apod); // this way if only 1 item is returned, there is still a valid `data.length` property
-    // console.log('data',data);
+    console.log('data',data);
 
     // create all the tile items for the html template
     for (var i = 1; i < data.length; i++) {
-      this.apodCards.push({ text: data[i].title, cols: 1, rows: 1, image: data[i].url, isCollapsed: true});
+      this.apodCards.push({ text: data[i].title, cols: 1, rows: 1, image: data[i].url, details: data[i].explanation, isCollapsed: true});
     }
   }
 
-  // TODO decouple ApodCardComponent
-  collapse(card : ApodCardComponent): void {
+  /**
+  * Changes the `isCollapsed` property of a particular
+  * `card` to the opposite of what it was. A collapsed
+  * card only shows the APOD image not the title.
+  *
+  * TODO decouple ApodCardComponent
+  */
+  collapse(card : var): void {
     console.log(card);
     card.isCollapsed = !card.isCollapsed;
   }
